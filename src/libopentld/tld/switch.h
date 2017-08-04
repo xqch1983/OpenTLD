a63ee36f4cfa144842910068bd5418b3ec00a485
xqch1983 <xieqingchun@126.com>
2017-08-04 14:45:26 +0800

    Add files via upload
    Try to add some switch options to selected running CPU or GPU (using OpenCL)

---
+++ src/libopentld/tld/switch.h
@@ -0,0 +1,14 @@
+#define opencl 1
+
+#define clDetect 1
+#define clIntegral 1
+#define clIntegral_m 1
+#define clIntegral_m_1 0
+#define cloverlap 0
+
+#define PrintTime_overlap 0
+#define PrintTime_detect 0
+#define PrintTime_learn 0
+#define PrintTime_integral 0
+#define PrintTime_fuseHypotheses 0
+#define SELECTED_DEVICE_ID 0
