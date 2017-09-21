9fa3963c9731821e7b77d46198e2a96e304ddb77
xqch1983 <xieqingchun@126.com>
2017-09-21 16:00:50 +0800

    modified the file named switch.h.

--- src/libopentld/tld/switch.h
+++ src/libopentld/tld/switch.h
@@ -1,17 +1,21 @@
-#define opencl 0
+#define opencl 1
 #if   opencl
 #define CPU 0
+#define clDetect 1
+#define clIntegral 1
+#define clIntegral_m 1
+#define clOverlap 0
+#define clNNClassifier  0
 #else 
 #define CPU 1
-#endif
-
 #define clDetect 0
 #define clIntegral 0
 #define clIntegral_m 0
-#define clIntegral_m_1 0
 #define clOverlap 0
 #define clNNClassifier  0
-
+#endif
+//#define clIntegral_m_1 0
+//#define clIntegral_m_1 0
 #define PrintTime_overlap 0
 #define PrintTime_detect 0
 #define PrintTime_learn 0
